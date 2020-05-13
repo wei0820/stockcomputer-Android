b713c6f71e7ce4f26767263157e8f2a10706f5f6
Wei Wei <jheng.wei0919@gmail.com>
2020-05-13 18:09:45 +0800

    add view add admob ad

--- app/src/main/java/com/jackpan/stockcomputer/MainActivity.kt
+++ app/src/main/java/com/jackpan/stockcomputer/MainActivity.kt
@@ -3,16 +3,24 @@ package com.jackpan.stockcomputer
 import android.app.Activity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 import com.wuhenzhizao.titlebar.widget.CommonTitleBar
 
 class MainActivity : Activity() {
-    lateinit var titleBar : com.wuhenzhizao.titlebar.widget.CommonTitleBar
+    lateinit var titleBar : CommonTitleBar
+    lateinit var mAdView : AdView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         titleBar = findViewById(R.id.titlebar)
         setTitleBar()
+        MobileAds.initialize(this) {}
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
     }
 
     fun setTitleBar(){
