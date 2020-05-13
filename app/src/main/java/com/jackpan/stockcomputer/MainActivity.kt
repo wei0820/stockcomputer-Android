f1842d158f1e0e4f9fdcbb02ebe710de1e4d1cd5
Wei Wei <jheng.wei0919@gmail.com>
2020-05-13 17:51:56 +0800

    set titlebar right lift text onclickListener

--- app/src/main/java/com/jackpan/stockcomputer/MainActivity.kt
+++ app/src/main/java/com/jackpan/stockcomputer/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.jackpan.stockcomputer
 
 import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.wuhenzhizao.titlebar.widget.CommonTitleBar
 
 class MainActivity : Activity() {
+    lateinit var titleBar : com.wuhenzhizao.titlebar.widget.CommonTitleBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        titleBar = findViewById(R.id.titlebar)
+        setTitleBar()
+    }
+
+    fun setTitleBar(){
+        titleBar.setListener { v, action, extra ->
+            when(action){
+                CommonTitleBar.ACTION_LEFT_TEXT -> Toast.makeText(this,"1",Toast.LENGTH_SHORT).show()
+                CommonTitleBar.ACTION_RIGHT_TEXT -> Toast.makeText(this,"2",Toast.LENGTH_SHORT).show()
+
+            }
+
+        }
+
     }
 }
