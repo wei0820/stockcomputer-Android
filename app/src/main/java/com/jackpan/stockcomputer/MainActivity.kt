d19f25e47296885cee1fdf4d80f02124d9550e91
Wei Wei <jheng.wei0919@gmail.com>
2020-05-13 17:16:54 +0800

    remove title bar add   Background color white

--- app/src/main/java/com/jackpan/stockcomputer/MainActivity.kt
+++ app/src/main/java/com/jackpan/stockcomputer/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.jackpan.stockcomputer
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
