a85a732825b3711df8926d7c07f245eb326c9aac
Wei Wei <jheng.wei0919@gmail.com>
2020-05-21 18:31:15 +0800

    add fun

--- app/src/main/java/com/jackpan/stockcomputer/MySharedPrefernces.java
+++ app/src/main/java/com/jackpan/stockcomputer/MySharedPrefernces.java
@@ -10,7 +10,7 @@ public class MySharedPrefernces {
     public static final String KEY_MEMBER_POINT= "memberpoint";
     public static final String KEY_LASTLOGINTIME= "lastlogintime";
     public static final String KEY_WATCHADTIME= "watchadtime";
-    
+
 
 
     public static void saveMemberPoint(Context context, Long ppint) {
@@ -22,6 +22,27 @@ public class MySharedPrefernces {
         SharedPreferences sp = context.getSharedPreferences(NAME, Activity.MODE_PRIVATE);
         return sp.getLong(KEY_MEMBER_POINT,0);
     }
+
+    public static void saveLastLoginTime(Context context, Long ppint) {
+        SharedPreferences sp = context.getSharedPreferences(NAME, Activity.MODE_PRIVATE);
+        sp.edit().putLong(KEY_LASTLOGINTIME, ppint).apply();
+    }
+
+    public static Long getLastLoginTime(Context context) {
+        SharedPreferences sp = context.getSharedPreferences(NAME, Activity.MODE_PRIVATE);
+        return sp.getLong(KEY_LASTLOGINTIME,0);
+    }
+
+    public static void saveWatchADTime(Context context, Long ppint) {
+        SharedPreferences sp = context.getSharedPreferences(NAME, Activity.MODE_PRIVATE);
+        sp.edit().putLong(KEY_WATCHADTIME, ppint).apply();
+    }
+
+    public static Long getWatchADTime(Context context) {
+        SharedPreferences sp = context.getSharedPreferences(NAME, Activity.MODE_PRIVATE);
+        return sp.getLong(KEY_WATCHADTIME,0);
+    }
+
 
 
 
