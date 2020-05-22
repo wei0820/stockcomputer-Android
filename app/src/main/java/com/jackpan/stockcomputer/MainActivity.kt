36cc27a38cf55ce05e29096e193226b3d9141f8b
Wei Wei <jheng.wei0919@gmail.com>
2020-05-22 17:56:19 +0800

    add layout fun

--- app/src/main/java/com/jackpan/stockcomputer/MainActivity.kt
+++ app/src/main/java/com/jackpan/stockcomputer/MainActivity.kt
@@ -5,6 +5,9 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import android.widget.Toast
 
 import com.google.android.gms.ads.AdRequest
@@ -19,8 +22,10 @@ import androidx.annotation.IdRes
 
 
 
-    class MainActivity : Activity() {
-            lateinit var titleBar : CommonTitleBar
+    class MainActivity : Activity(), View.OnClickListener
+    {
+
+        lateinit var titleBar : CommonTitleBar
             lateinit var mAdView : AdView
             lateinit var MarqueeTextView :com.xiaweizi.marquee.MarqueeTextView
             lateinit var mXBanner : Banner
@@ -28,37 +33,63 @@ import androidx.annotation.IdRes
         val bottomBar_int_1 : Int = 2131231004
         val bottomBar_int_2 : Int = 2131231006
         val bottomBar_int_3 : Int = 2131231005
+        lateinit var mLayout: RelativeLayout
+        lateinit var mLayout2: RelativeLayout
+        lateinit var mLayout3: RelativeLayout
+        lateinit var mLayout4: RelativeLayout
+        lateinit var mLayout5: RelativeLayout
+        lateinit var mLayout6: RelativeLayout
+
 
         var mArray  = arrayListOf<String>()
             override fun onCreate(savedInstanceState: Bundle?) {
                 super.onCreate(savedInstanceState)
                 setContentView(R.layout.activity_main)
-                titleBar = findViewById(R.id.titlebar)
-                mXBanner = findViewById(R.id.banner)
-                MarqueeTextView = findViewById(R.id.marquee1)
-                mBottomBar = findViewById(R.id.bottomBar)
+                initLayout()
                 setTitleBar()
-                MobileAds.initialize(this) {}
-                mAdView = findViewById(R.id.adView)
-                val adRequest = AdRequest.Builder().build()
-                mAdView.loadAd(adRequest)
+                setAd()
                 FirebaseDatebaseManager.getFirebaseDatebase(MarqueeTextView)
                 FirebaseDatebaseManager.getBannerData(mXBanner)
-                mBottomBar.setOnTabSelectListener {
-                    when(it){
-                        bottomBar_int_1 -> Log.d("Jack","1")
-                        bottomBar_int_2 -> Log.d("Jack","2")
-                        bottomBar_int_3 -> Log.d("Jack","3")
 
-                    }
-
-                }
 
             }
 
 
 
+    fun initLayout(){
+        mLayout = findViewById(R.id.layout_1)
+        mLayout2 = findViewById(R.id.layout_2)
+        mLayout3 = findViewById(R.id.layout_3)
+        mLayout4 = findViewById(R.id.layout_4)
+        mLayout5 = findViewById(R.id.layout_5)
+        mLayout6 = findViewById(R.id.layout_6)
+        mLayout.setOnClickListener(this)
+        mLayout2.setOnClickListener(this)
+        mLayout3.setOnClickListener(this)
+        mLayout4.setOnClickListener(this)
+        mLayout5.setOnClickListener(this)
+        mLayout6.setOnClickListener(this)
 
+        titleBar = findViewById(R.id.titlebar)
+        mXBanner = findViewById(R.id.banner)
+        MarqueeTextView = findViewById(R.id.marquee1)
+        mBottomBar = findViewById(R.id.bottomBar)
+        mBottomBar.setOnTabSelectListener {
+            when(it){
+                bottomBar_int_1 -> Log.d("Jack","1")
+                bottomBar_int_2 -> Log.d("Jack","2")
+                bottomBar_int_3 -> Log.d("Jack","3")
+
+            }
+
+        }
+    }
+        fun setAd(){
+            MobileAds.initialize(this) {}
+            mAdView = findViewById(R.id.adView)
+            val adRequest = AdRequest.Builder().build()
+            mAdView.loadAd(adRequest)
+        }
     fun setTitleBar(){
         titleBar.setListener { v, action, extra ->
             when(action){
@@ -71,5 +102,19 @@ import androidx.annotation.IdRes
         }
 
     }
+        override fun onClick(p0: View?) {
+            when(p0!!.id){
+                R.id.layout_1 ->""
+                R.id.layout_2 ->""
+                R.id.layout_3 ->""
+                R.id.layout_4 ->""
+                R.id.layout_5 ->""
+                R.id.layout_6 ->""
 
-}
+            }
+
+
+        }
+
+
+    }
