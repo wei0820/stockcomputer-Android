d469d28f440f84a21a10ad61b936d989e0b9769e
mac <jheng.wei0919@gmail.com>
2021-04-09 13:15:47 +0800

    fix function

--- app/src/main/java/com/jackpan/stockcomputer/member/Model/MemberModel.kt
+++ app/src/main/java/com/jackpan/stockcomputer/member/Model/MemberModel.kt
@@ -1,11 +1,18 @@
 package com.jackpan.stockcomputer.member.Model
+import android.app.Activity
 import android.app.Application
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
 import com.jackpan.stockcomputer.Model.PhoneLogin
 import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
 
 
 class MemberModel @ViewModelInject constructor(application: Application) : AndroidViewModel(application){
@@ -14,7 +21,23 @@ class MemberModel @ViewModelInject constructor(application: Application) : Andro
 
 
 
-    fun userPhoneLogin() = viewModelScope.launch {
+    fun userPhoneLogin(auth:FirebaseAuth,phoneNumber :String,activity: Activity) = viewModelScope.launch {
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(phoneNumber)       // Phone number to verify
+            .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+            .setActivity(activity)                 // Activity (for callback binding)
+            .setCallbacks(object  :PhoneAuthProvider.OnVerificationStateChangedCallbacks(){
+                override fun onVerificationCompleted(p0: PhoneAuthCredential) {
+                }
+
+                override fun onVerificationFailed(p0: FirebaseException) {
+                }
+
+            })          // OnVerificationStateChangedCallbacks
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+
+
 
 
 
